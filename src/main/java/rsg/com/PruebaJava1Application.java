eee524c1639638e7fb3618b360e96d7d89e6d5c7
rubensg <ruben.silva.garcia@live.com>
2023-02-09 12:53:43 -0600

    primer push

---
+++ src/main/java/rsg/com/PruebaJava1Application.java
@@ -0,0 +1,20 @@
+package rsg.com;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaJava1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaJava1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/rsg/com/PruebaJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rsg.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
